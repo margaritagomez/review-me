c995f8deda6ffc6b8c771a4fb361f1494e2f3b36
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-07 00:11:27 -0500

    Comentario general componentes

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -4,3 +4,5 @@ import App from './components/app'
 
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
+
+/*En general todos los componentes y su estructura es muy clara y simple. Es un muy buen trabajo de dividir en componentes los elementos de la vista :) */
